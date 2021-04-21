bd8889d4dc457f573dee3ad900c9d1fd6b5b92c9
ekafila <ekaterina.filiugina@gmail.com>
2021-04-21 22:45:53 +0300

    hw3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 
 const Task = ({id, name, description, completed}) => {
 
-  
   const TaskLog = (id, completed) => {
     console.log('Task ' + id + ' completed status = ' + completed);
   }
